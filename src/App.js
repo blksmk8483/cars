28866f42f4bb43fa2002558d7976fbd6646082fa
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-27 10:02:03 -0600

    added new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import CarForm from './components/CarForm';
+import CarList from './components/CarList';
+import CarSearch from './components/CarSearch';
+import CarValue from './components/CarValue';
+
+
 function App() {
-    return <div>hello there...</div>;
+    return (
+    <div>
+        <CarForm />
+        <CarList />
+        <CarSearch />
+        <CarValue />
+    </div>
+    );
 }
 
 export default App;
